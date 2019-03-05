beee8e78580a9cde9e623ae9a8bb651c2c5a4348
Juan Vento <j.j.v.sevilla@gmail.com>
2019-03-05 08:14:49 -0500

    add sample for circleci, circleci yml config

--- actions.go
+++ actions.go
@@ -10,29 +10,48 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var accessTokenKey = "Private-Token"
-var accessTokenValue = "PRIVATE_TOKEN_GOES_HERE"
+const gitLabAccessTokenKey = "Private-Token"
+const gitLabAccessTokenValue = "GITLAB_PRIVATE_TOKEN_GOES_HERE"
+const circleCIAPIToken = "CIRCLECI_PRIVATE_TOKEN_GOES_HERE"
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Home")
 }
 
-func Me(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Me")
+func GetCircleCIProjects(w http.ResponseWriter, r *http.Request) {
+	url := "https://circleci.com/api/v1.1/projects?circle-token=" + circleCIAPIToken
+
+	// Create new request for Gitlab
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal("Error reading request. ", err)
+	}
+
+	// Set headers
+	req.Header.Set("Cache-Control", "no-cache")
+
+	// Set client timeout
+	client := &http.Client{
+		Timeout: time.Second * 10,
+	}
+
+	// Send request to Gitlab
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Error reading response. ", err)
+	}
+	defer resp.Body.Close()
+
+	// Reading Gitlab's response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("Error reading body. ", err)
+	}
+
+	respondJSON(w, http.StatusOK, body)
 }
 
-func GetUser(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	username := params["username"]
-
-	fmt.Fprintf(w, "You want to fetch information from user "+username)
-}
-
-func GetProjects(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Projects")
-}
-
-func GetProject(w http.ResponseWriter, r *http.Request) {
+func GetGitLabProject(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	projectID := params["projectId"]
 	url := "http://gitlab.com/api/v4/projects/" + projectID
@@ -44,7 +63,7 @@ func GetProject(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Set headers
-	req.Header.Set(accessTokenKey, accessTokenValue)
+	req.Header.Set(gitLabAccessTokenKey, gitLabAccessTokenValue)
 	req.Header.Set("Cache-Control", "no-cache")
 
 	// Set client timeout
--- routes.go
+++ routes.go
@@ -7,9 +7,7 @@ import (
 func NewRouter() *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", Index).Methods("GET")
-	router.HandleFunc("/me", Me).Methods("GET")
-	router.HandleFunc("/users/{username}", GetUser).Methods("GET")
-	router.HandleFunc("/projects", GetProjects).Methods("GET")
-	router.HandleFunc("/projects/{projectId}", GetProject).Methods("GET")
+	router.HandleFunc("/gitlab/projects/{projectId}", GetGitLabProject).Methods("GET")
+	router.HandleFunc("/circleci/projects", GetCircleCIProjects).Methods("GET")
 	return router
 }
